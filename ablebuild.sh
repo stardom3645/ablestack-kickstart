47e6f7231f0ef14be6ca0ad38360e9256e31249b
ktcheon <ktc7270@gmail.com>
2021-04-05 12:23:27 +0900

    kickstart 스크립트 수정(cockpit branding) 및 ipcorrector 추가

--- ablebuild.sh
+++ ablebuild.sh
@@ -41,7 +41,7 @@ yes|cp $PWD_PATH/kickstart/rpms/* $KS_PATH/rpms/
 yes|cp $PWD_PATH/kickstart/whls/* $KS_PATH/whls/
 yes|cp $PWD_PATH/kickstart/scripts/* $KS_PATH/scripts
 yes|cp $PWD_PATH/kickstart/settings/cockpit/* $KS_PATH/settings/cockpit/
-yes|cp $PWD_PATH/kickstart/settings/images/* $KS_PATH/settings/images/
+#yes|cp $PWD_PATH/kickstart/settings/images/* $KS_PATH/settings/images/
 
 
 mkisofs -o $PWD_PATH/ablestack_$1-el8.iso -b isolinux/isolinux.bin -J -R -l -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -graft-points -r -V "CentOS-8-3-2011-x86_64-dvd" $KS_PATH
